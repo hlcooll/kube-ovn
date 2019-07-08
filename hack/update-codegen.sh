0155d6b0e2c5cc9529ba8fc576cdd977e8d46d47
oilbeater <liumengxinfly@gmail.com>
2019-07-08 17:32:13 +0800

    ci

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -10,6 +10,6 @@ CODEGEN_PKG=$GOPATH/src/k8s.io/code-generator
 # --output-base    because this script should also be able to run inside the vendor dir of
 #                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
 #                  instead of the $GOPATH directly. For normal projects this can be dropped.
-${CODEGEN_PKG}/generate-groups.sh "deepcopy" \
+${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
   github.com/alauda/kube-ovn/pkg/client github.com/alauda/kube-ovn/pkg/apis \
   kubeovn:v1
